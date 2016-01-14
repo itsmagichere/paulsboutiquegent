9751f45fa63cc4304717cdc114cf7cb73f22745e
ramsaysewell <admin@ramsay.in>
2016-01-14 18:41:10 +0000

    My favourite commit so far!

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    
+   $('#bread').hover(function(){
+    $(this).attr('src','img/assets/PNGs/breadcolour.png');
+},function(){
+     $(this).attr('src','img/assets/PNGs/bread.png'); 
+});
+    $('#pig').hover(function(){
+    $(this).attr('src','img/assets/PNGs/pigcolour.png');
+},function(){
+     $(this).attr('src','img/assets/PNGs/pig.png'); 
+});
+    $('#cheese').hover(function(){
+    $(this).attr('src','img/assets/PNGs/cheesecolour.png');
+},function(){
+     $(this).attr('src','img/assets/PNGs/cheese.png'); 
+});
+    $('#salad').hover(function(){
+    $(this).attr('src','img/assets/PNGs/saladcolour.png');
+},function(){
+     $(this).attr('src','img/assets/PNGs/salad.png'); 
+});
+});
